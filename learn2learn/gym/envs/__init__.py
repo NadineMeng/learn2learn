c3d5fbab2499096b9f7874b260d49621f1ff46e6
NadineMeng <mjw00096@gmail.com>
2022-05-20 22:08:34 +0200

    add cartpole env

--- learn2learn/gym/envs/__init__.py
+++ learn2learn/gym/envs/__init__.py
@@ -13,12 +13,45 @@ register(
     max_episode_steps=100
 )
 
+
+
+# Classic Control
+# ------------------------------------------
+register(
+    id='CartPole-Dis-v1',
+    entry_point='learn2learn.gym.envs.classic_control.cartpole:MetaCartPoleEnv',
+    max_episode_steps=200,
+    #reward_threshold=195.0,
+)
+
+register(
+    id='Cartpole-Conti-v1',
+    entry_point='learn2learn.gym.envs.classic_control.cartpole_continuous:MetaContinuousCartPoleEnv',
+    max_episode_steps=200,
+    #reward_threshold=195.0,
+)
+
+
+
+
 # Mujoco
 # ----------------------------------------
 
 register(
     'HalfCheetahForwardBackward-v1',
     entry_point='learn2learn.gym.envs.mujoco.halfcheetah_forward_backward:HalfCheetahForwardBackwardEnv',
+    max_episode_steps=100,
+)
+
+register(
+    'HalfCheetahRandVel-v1',
+    entry_point='learn2learn.gym.envs.mujoco.halfcheetah_velocity:HalfCheetahRandVelEnv',
+    max_episode_steps=100,
+)
+
+register(
+    'HalfCheetahRandGra-v1',
+    entry_point='learn2learn.gym.envs.mujoco.halfcheetah_gravity:HalfCheetahGraEnv',
     max_episode_steps=100,
 )
 
@@ -43,5 +76,11 @@ register(
 register(
     'HumanoidDirection-v1',
     entry_point='learn2learn.gym.envs.mujoco.humanoid_direction:HumanoidDirectionEnv',
+    max_episode_steps=200,
+)
+
+register(
+    'WalkerVel-v1',
+    entry_point='learn2learn.gym.envs.mujoco.walker2d_rand_vel:Walker2DRandVelEnv',
     max_episode_steps=200,
 )
